2fe7f8aaa4b0f04b6c406cec8931898a4c9f461d
Vitaliy Pogodin <pogodin.vitaliy.s@gmail.com>
2023-11-24 16:28:49 +0100

    striim-create-new-app.cy.ts is added(without POM)

--- cypress/e2e/striim-create-new-app.cy.ts
+++ cypress/e2e/striim-create-new-app.cy.ts
@@ -3,7 +3,7 @@ describe("How to display #landing page",() => {
   beforeEach (() => {
     cy.visit('https://developer.striim.com')
     // 1.Have to wait!!!
-    cy.wait(10000)
+    cy.wait(7000)
   })
 
   it('login actions + handling of mistake + landing page',() => {
@@ -35,11 +35,29 @@ describe("How to display #landing page",() => {
     cy.get('h1').eq(0).should('have.text',`Hi ${logIn}, Welcome to Striim !`)
     // or this
     cy.get('div[data-test-id="header--homepage-recent-apps-empty"] h1').eq(0).should('have.text',`Hi ${logIn}, Welcome to Striim !`)
+
+    //Click  button to create new app if it exists
+    cy.get('button[data-test-id="create-an-app--button"]').should('exist').click()
+
+    //Check URL for creating new app
+    cy.url().should('eq','https://developer.striim.com/#create-app')
+
+    //Find element for creating from scratch and click
+    cy.get('div#createapp--Start_from_scratch').should('exist').click()
+
+    //Check URL for page with giving name to new app
+    cy.url().should('eq','https://developer.striim.com/#create-app/name-app')
+
+    //Name app
+    cy.get('input[name="name"]').type(`qqqq`).should('have.value',`qqqq`)
+
+    //Click button for submit new app
+    cy.get('button[data-test-id="submit-app-button"]').should('exist').click()
+
+    //Check URL for work with new app :Url is OK, but picture is different
+    cy.url().should('eq','https://developer.striim.com/#flow/VitaliyP.qqqq')
     
   })
 
 })
-
-
-
 
